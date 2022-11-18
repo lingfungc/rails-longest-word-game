0ae3e8ab9d31c1666ea67e09902464be01ba58ff
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-18 22:22:49 +0000

    completed #new method and displayed 10 letters in new view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
   def new
+    @letters = []
+    10.times do
+      letter = ('A'..'Z').to_a.sample
+      @letters << letter
+    end
   end
 
   def score
