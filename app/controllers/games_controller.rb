9262b961c2d87ba55d96b65d00116241613b8917
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-19 16:50:20 +0000

    updated routes for /score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -8,5 +10,23 @@ class GamesController < ApplicationController
   end
 
   def score
+    # @letters = params[:letters].split
+    # @word = (params[:word] || "").upcase
+    # @included = included?(@word, @letters)
+    # @english_word = english_word?(@word)
+    @params = params
+    # raise
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
