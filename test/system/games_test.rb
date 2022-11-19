d768f1296f552a73b300ed3d8603c4651bb68264
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-19 18:46:34 +0000

    added a simple test for generating 10 letters in new.html.erb

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -4,6 +4,6 @@ class GamesTest < ApplicationSystemTestCase
   test "Going to /new gives us a new random grid to play with" do
     visit new_url
     assert test: "New game"
-    assert_selector "li", count: 10
+    assert_selector 'h4', count: 10
   end
 end
