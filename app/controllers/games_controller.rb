c623b70553fc9bd7859f912c3e6c9f228190d0f3
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-19 18:09:03 +0000

    added new logic to generate @letters with vowels

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,17 @@
 require 'open-uri'
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = []
-    10.times do
-      letter = ('A'..'Z').to_a.sample
-      @letters << letter
-    end
+    # @letters = []
+    # 10.times do
+    #   letter = ('A'..'Z').to_a.sample
+    #   @letters << letter
+    # end
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
